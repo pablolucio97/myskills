36488f72b916d9303602eedb9ff80e8ec372e9c4
pablolucio97 <pablolucio97@users.noreply.github.com>
2022-03-15 12:33:12 -0300

    💎: Added splash screen at app loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Home } from './src/pages/Home'
+import SplashScreen from 'react-native-splash-screen'
 
-export default function App(){
-    return(
+export default function App() {
+
+    useEffect(() => {
+         SplashScreen.hide()
+    }, )
+
+    return (
         <Home />
     )
 }
